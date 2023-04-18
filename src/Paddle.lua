dec495b2ba09af5fc1c1beccce5d5a8ca931f959
shima8823 <taiyaki1275@gmail.com>
2023-04-18 20:26:18 +0900

    paddleの初期値を変更

--- src/Paddle.lua
+++ src/Paddle.lua
@@ -30,7 +30,7 @@ function Paddle:init(skin)
     self.dx = 0
 
     -- starting dimensions
-    self.width = 64
+    self.width = 96
     self.height = 16
 
     -- the skin only has the effect of changing our color, used to offset us
@@ -39,7 +39,7 @@ function Paddle:init(skin)
 
     -- the variant is which of the four paddle sizes we currently are; 2
     -- is the starting size, as the smallest is too tough to start with
-    self.size = 2
+    self.size = 3
 end
 
 function Paddle:update(dt)
